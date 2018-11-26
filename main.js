d70c2e3bf2349177e74ac2cf234c315f47a3daf5
LeonardoSarallo <leo.sarallo@gmail.com>
2018-11-26 14:32:47 +0100

    provasenzafunzione

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-alert('ciao')
+// Un alert espone 5 numeri casuali. Da li parte un
+// timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire un prompt
+// alla volta i numeri che ha visto precedentemente.
+// Dopo che sono stati inseriti i 5 numeri, il software
+// dice quanti e quali dei numeri da indovinare sono
+// stati individuati
+
+function getRandomNumber(min, max) {
+  for (var i = 0; i < 5; i++)
+  {
+    var random = Math.floor(Math.random()*(max-min+1)+min);
+    console.log(random);
+
+  }
+  return random;
+}
+
+alert(getRandomNumber(0,9))
+console.log(alert);
